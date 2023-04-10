996e2942d465b6ad254f3400355174a27f1b902d
Gabriel <gabrielalves3654@gmail.com>
2023-04-10 12:00:00 -0300

    padronizando os dados

--- db/createDB.py
+++ db/createDB.py
@@ -11,14 +11,14 @@ conn = sqlite3.connect('unas_banco_de_dados.db')
 cursor = conn.cursor()
 
 # Criar uma tabela no banco de dados
-cursor.execute('''CREATE TABLE IF NOT EXISTS Tabela_UNAS (id INTEGER PRIMARY KEY, nome TEXT, idade TEXT, sexo TEXT, projeto TEXT, raca TEXT, bairro TEXT )''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS Tabela_UNAS (id INTEGER PRIMARY KEY, projeto TEXT, nome TEXT, idade TEXT, sexo TEXT, raca TEXT, bairro TEXT )''')
 
 # Ler as linhas do arquivo do Excel e inserir na tabela do banco de dados
 for arquivo in arquivos_excel:
     workbook = openpyxl.load_workbook(arquivo)
     sheet = workbook.active
     for row in sheet.iter_rows(min_row=2, values_only=True):
-        cursor.execute("INSERT INTO Tabela_UNAS (projeto, nome, idade, sexo, raca, bairro) VALUES (?, ?, ?, ?, ?, ?)", (row[1], row[2], row[3], row[4], row[5], row[6]))
+        cursor.execute("INSERT INTO Tabela_UNAS (projeto, nome, idade, sexo, raca, bairro) VALUES (?, ?, ?, ?, ?, ?)", (row[1], row[2].upper(), row[3], row[4], row[5], row[6]))
 
 # Salvar as alterações e fechar a conexão com o banco de dados
 conn.commit()
--- db/readDB.py
+++ db/readDB.py
@@ -5,8 +5,7 @@ conn = sqlite3.connect('/Users/gabrielalves/projetos-pessoais/Unas-db/unas_banco
 cursor = conn.cursor()
 
 # Executar uma consulta SQL para selecionar todos os dados da tabela
-cursor.execute("SELECT COUNT(*) FROM Tabela_UNAS ")
-
+cursor.execute("SELECT COUNT(*) FROM Tabela_UNAS WHERE projeto = 'CEI PAULO FREIRE'")
 # Recuperar e imprimir todos os resultados da consulta
 rows = cursor.fetchall()
 for row in rows:
